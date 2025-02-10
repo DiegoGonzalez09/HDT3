bdafdae48668f0e58b0ee112aaedef092b7911f8
DiegoGonzalez09 <gon241214@uvg.edu.gt>
2025-02-10 11:11:42 -0600

    Update Main.java

--- scr/main/java/org/example/Main.java
+++ scr/main/java/org/example/Main.java
@@ -1,1 +1,31 @@
 
+package org.example;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        SortingAlgorithms sorter = new SortingAlgorithms(); // Única instancia
+
+        // Tamaños de arrays a probar
+        final int[] sizes = {10, 100, 500, 1000, 2000, 3000};
+
+        for (int size : sizes) {
+            System.out.println("Testing with size: " + size);
+
+            // Generar números aleatorios
+            sorter.generateRandomNumbers(size);
+
+            // Probar cada algoritmo
+            testAlgorithm("Insertion Sort", sorter, SortingAlgorithms::insertionSort);
+            testAlgorithm("Merge Sort", sorter, SortingAlgorithms::mergeSort);
+            testAlgorithm("Quick Sort", sorter, SortingAlgorithms::quickSort);
+            testAlgorithm("Radix Sort", sorter, SortingAlgorithms::radixSort);
+            testAlgorithm("Bucket Sort", sorter, SortingAlgorithms::bucketSort);
+            testAlgorithm("Heap Sort", sorter, SortingAlgorithms::heapSort);
+
+            System.out.println();
+        }
+    }
